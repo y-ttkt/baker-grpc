143785e1b071c814379259a9373635b6d5cd8037
y-ttkt <github ys.taka44@gmail.com>
2025-09-13 09:04:27 +0900

    feat:add uber

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,8 @@ module github.com/y-ttkt/baker
 go 1.21.5
 
 require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
--- api/server/server.go
+++ api/server/server.go
@@ -19,16 +19,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	server := grpc.NewServer()
 	api.RegisterPancakeBakerServiceServer(
 		server,
 		handler.NewBakerHandler(),
 	)
+
 	reflection.Register(server)
+
 	go func() {
 		log.Printf("start gRPC server port: %v", port)
 		server.Serve(lis)
 	}()
+
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
